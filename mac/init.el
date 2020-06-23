236ce4548c3f462abd54cf3e7ca190af486c4345
fuminori-kobayashi <genhooha@gmail.com>
2020-06-24 08:32:24 +0900

    update

--- mac/init.el
+++ mac/init.el
@@ -1,111 +1,296 @@
-;; 起動画面を表示しない
-(setq inhibit-startup-message t)
+;;; init.el --- Emacs個人設定
 
-;; ツールバー非表示
-(tool-bar-mode 0)
+;;; Commentary:
 
-;; CommandとOptionを入れ替える
-(setq ns-command-modifier (quote meta))
-(setq ns-alternate-modifier (quote super))
+;;; Code:
+;; ctrl-h で backspace
+(global-set-key "\C-h" 'delete-backward-char)
 
-;;backspace
-(global-set-key "\C-h" 'backward-delete-char)
+;;; load-pathの追加関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
 
-;; 日本語IM用の設定
-(setq default-input-method "MacOSX")
-
-;; 日本語の設定（UTF-8）
-(set-language-environment 'Japanese)
-(prefer-coding-system 'utf-8)
-(custom-set-variables
-
-;; custom-set-variables was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tsdh-dark))))
-(custom-set-faces )
-
-;;パッケージリポジトリ追加
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; ;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; ;; MELPA-stableを追加
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; ;; Marmaladeを追加
+;; (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+;; ;; Orgを追加
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; ;; 初期化
 (package-initialize)
 
-;;行番号
-(global-linum-mode t)
+;; load-pathに追加するフォルダ
+;; 2つ以上指定する場合の形 -> (add-to-load-path "elisp" "xxx" "xxx")
+;; $ mkdir ~/.emacs/elisp
+;;(add-to-load-path "elisp")
 
-;; eww
-(setq eww-search-prefix "https://www.google.co.jp/search?q=")
+;;; スタートアップ非表示
+(setq inhibit-startup-screen t)
 
-;; 自動バックアップ設定
+;;; toolbar/menubar
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+
+
+;;; ファイルのフルパスをタイトルバーに表示
+(setq frame-title-format
+      (format "%%f - Emacs@%s" (system-name)))
+
+;;; Windows で英数に DejaVu Sans Mono、日本語にMeiryoを指定
+;; (when (eq window-system 'w32)
+;;   (set-face-attribute 'default nil
+;;                       :family "DejaVu Sans Mono"
+;;                       :height 100)
+;;   (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Meiryo")))
+
+;;; バックアップを残さない
 (setq make-backup-files nil)
 
-;; ls 対策 --- http://qiita.com/maangie/items/5a80ae50c13d14368a72
-(let ((gls "/usr/local/bin/gls"))
-  (if (file-exists-p gls) (setq insert-directory-program gls)))
+;;; 行番号表示
+(global-linum-mode)
+;; 行番号
+(line-number-mode t)
+;; 列番号
+(column-number-mode t)
 
-;;かっこをハイライト
+;;; 文字コード
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(setq locale-coding-system 'utf-8)
+;;(setq file-name-coding-system 'sjis)
+
+;;----------------
+;; 括弧
+;;----------------
+;;; 括弧の範囲内を強調表示
+(setq show-paren-delay 0)
 (show-paren-mode t)
-; highlight entire bracket expression
 (setq show-paren-style 'expression)
 
+;; 括弧の範囲色
+;;(set-face-background 'show-paren-match-face "#804")
+
+;; かっこの自動補完
+;;http://ergoemacs.org/emacs/emacs_insert_brackets_by_pair.html
+(electric-pair-mode 1)
+
+;; 左括弧の後に右括弧が入力されたときにカーソルを括弧の中に移動させるマイナーモード cursor-in-brackets.el
+;; http://d.hatena.ne.jp/yascentur/20130526/1369550512
+;; (when (require 'cursor-in-brackets nil t)
+;;   (global-cursor-in-brackets-mode t))
+
+;;----------------
+;; インデント
+;;----------------
+
+;; タブにスペースを使用する
+;; tab width
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+
+;; javascript mode
+(setq js-indent-level 2)
+
+;;----------------
+;; インデント
+;;----------------
+
+;;; 選択領域の色
+(set-face-background 'region "#36f")
+
+
+;;; 最近使ったファイルをメニューに表示
+(recentf-mode 1)
+(setq recentf-max-menu-items 10)
+(setq recentf-max-saved-items 10)
+
+;;通常のウィンドウ用の設定
+(setq-default truncate-lines t)
+;;ウィンドウを左右に分割したとき用の設定
+(setq-default truncate-partial-width-windows nil)
+;;; ツールバー非表示
+;;(tool-bar-mode 0)
+
+;; 画面の３分割
+(defun split-window-vertically-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-vertically)
+    (progn
+      (split-window-vertically
+       (- (window-height) (/ (window-height) num_wins)))
+      (split-window-vertically-n (- num_wins 1)))))
+(defun split-window-horizontally-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-horizontally)
+    (progn
+      (split-window-horizontally
+       (- (window-width) (/ (window-width) num_wins)))
+      (split-window-horizontally-n (- num_wins 1)))))
+(global-set-key "\C-x@" '(lambda ()
+                           (interactive)
+                           (split-window-vertically-n 3)))
+(global-set-key "\C-x#" '(lambda ()
+                           (interactive)
+                           (split-window-horizontally-n 3)))
+
+
+;; "<M-kanji> is undefined"を非表示
+(global-set-key [M-kanji] 'ignore)
+
 ;;====================================
-;; SHOW-CSS
+;;; 折り返し表示ON/OFF
 ;;====================================
-(autoload 'showcss-mode "show-css"
-   "Display the css of the class or id the cursor is at" t)
-(defun sm/toggle-showcss()
-  "Toggle showcss-mode"
+(setq truncate-partial-width-windows nil)
+(defun toggle-truncate-lines ()
+  "折り返し表示をトグル動作します."
   (interactive)
-  (if (derived-mode-p
-       'html-mode
-       'nxml-mode
-       'nxhtml-mode
-       'web-mode
-       'handlebars-mode)
-      (showcss-mode 'toggle)
-    (message "Not in an html mode")))
-(global-set-key (kbd "C-c C-k") 'sm/toggle-showcss)
+  (if truncate-lines
+      (setq truncate-lines nil)
+    (setq truncate-lines t))
+  (recenter))
 
-;;====================================
-;; web-mode
-;;====================================
+(global-set-key "\C-c\C-l" 'toggle-truncate-lines) ; 折り返し表示ON/OFF
 
+;;
+;; Package setting
+;;
+
+;;-------------------------
+;; WEB mode --- http://web-mode.org/
+;;
+;; [install]
+;; M-x package-install => web-mode
+;;-------------------------
 (require 'web-mode)
-
-;;; emacs 23以下の互換
-;; (when (< emacs-major-version 24)
-;;   (defalias 'prog-mode 'fundamental-mode))
-
-;;; 適用する拡張子
-;; (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
-
-;;; インデント数
-(defun web-mode-hook ()
+(add-to-list 'auto-mode-alist '("\\.php\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.js\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'"   . web-mode))
+(defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-html-offset   2)
-  (setq web-mode-css-offset    2)
-  (setq web-mode-script-offset 2)
-  (setq web-mode-php-offset    2)
-  (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2))
-(add-hook 'web-mode-hook 'web-mode-hook)
+  ;; HTML element offset indentation
+  (setq web-mode-markup-indent-offset 2)
+  ;; CSS offset indentation
+  (setq web-mode-css-indent-offset 2)
+  ;;Script/code offset indentation (for JavaScript, Java, PHP, Ruby, Go, VBScript, Python, etc.)
+  (setq web-mode-code-indent-offset 2)
+  ;;For <style> parts
+  (setq web-mode-style-padding 2)
+  ;;For <script> parts
+  (setq web-mode-script-padding 2)
+  ;;For multi-line blocks
+  (setq web-mode-block-padding 0)
 
-;;====================================
-;; auto-complete
-;;====================================
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;;-------------------------------------
+;; flycheck --- https://www.flycheck.org/en/latest/
+;; 編集中のlintツール連携
+;;
+;; [install]
+;; M-x package-list-package => flycheck
+;;-------------------------------------
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+          '(json-jsonlist)))
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; use eslint with web-mode
+;; https://eslint.org/
+;; [install]
+;; sudo npm install -g eslint
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; use html-tidy whth web-mode
+;; http://www.html-tidy.org/
+;; [install] http://binaries.html-tidy.org/
+;; wget (rpm download url)
+;; sudo rpm -ihv (downloaded rpm filename)
+;; (flycheck-add-mode 'html-tidy 'web-mode)
 
 
-(require 'auto-complete)
-(require 'auto-complete-config)
-(global-auto-complete-mode t)
-(define-key ac-completing-map (kbd "C-n") 'ac-next)
-(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+;;-------------------------------------
+;; Prettier --- https://github.com/prettier/prettier-emacs
+;; script 整形
+;;
+;; [install]
+;; shell) sudo npm install -g prettier
+;; emacs) M-x package-install => prettier-js
+;;-------------------------------------
 
+(require 'prettier-js)
+
+;;-------------------------------------
+;; company --- https://company-mode.github.io/
+;; 補完
+;;
+;; [install]
+;; M-x package-install => company
+;;-------------------------------------
+(require 'company)
+(global-company-mode) ; 全バッファで有効にする 
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
+;;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rjsx-mode typescript-mode web-mode prettier-js flycheck company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;;
+;;-------------------------------------
+;; rjsx-mode
+;; react
+;;
+;; [install]
+;; M-x package-install => rjsx-mode
+;;-------------------------------------
+
+(add-to-list 'auto-mode-alist '(".*\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("containers\\/.*\\.js\\'" . rjsx-mode))
+(add-hook 'rjsx-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil) ;;インデントはタブではなくスペース
+            (setq js-indent-level 2) ;;スペースは２つ、デフォルトは4
+            (setq js2-strict-missing-semi-warning nil))) ;;行末のセミコロンの警告はオフ
 
